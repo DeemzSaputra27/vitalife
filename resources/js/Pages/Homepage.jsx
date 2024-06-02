61026de897bc3bb9a4a4c8a5a379082017c04011
DeemzSaputra27 <165003278+DeemzSaputra27@users.noreply.github.com>
2024-06-02 11:45:55 +0700

    Update Homepage.jsx

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link, Head } from '@inertiajs/react';
 
 export default function Homepage({ auth, laravelVersion, phpVersion }) {
-    // const handleImageError = () => {
-    //     document.getElementById('screenshot-container')?.classList.add('!hidden');
-    //     document.getElementById('docs-card')?.classList.add('!row-span-1');
-    //     document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    //     document.getElementById('background')?.classList.add('!hidden');
-    // };
+    const handleImageError = () => {
+        document.getElementById('screenshot-container')?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document.getElementById('docs-card-content')?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
 
     return (
 
