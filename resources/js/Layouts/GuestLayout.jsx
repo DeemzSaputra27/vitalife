5b9d3ebd1442976ccd86acb4a6f46b022abdc2b2
DeemzSaputra27 <2211501033@student.unisayogya.ac.id>
2024-06-04 23:33:33 +0700

    commit

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,9 +3,9 @@ import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style={{ backgroundImage: 'url(https://cdn.easyfrontend.com/pictures/sign-in-up/sign-in-up-1.png)', backgroundSize: 'cover' }}>
+        <div className="min-h-screen flex flex-col justify-center sm:justify-end items-center pt-6 sm:pt-0" style={{ backgroundImage: 'url(https://cdn.easyfrontend.com/pictures/sign-in-up/sign-in-up-1.png)', backgroundSize: 'cover' }}>
             <div>
-                <Link href="/">
+                <Link>
                     <ApplicationLogo className="w-20 h-20" />
                 </Link>
             </div>
