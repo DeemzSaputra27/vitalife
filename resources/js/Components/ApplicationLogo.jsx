cf1e74abb093d9598c6eaf8a2a67f0380cc97d9d
DeemzSaputra27 <165003278+DeemzSaputra27@users.noreply.github.com>
2024-06-11 12:06:03 +0700

    commit

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,7 +1,11 @@
 export default function ApplicationLogo(props) {
     return (
         <svg {...props} viewBox="0 0 316 316">
-            <image src="../image/LOGO_2.png" width="100%" height="100%" />
+            <img
+                src={require("../image/LOGO_2.png")}
+                width="100%"
+                height="100%"
+            />
         </svg>
     );
 }
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,9 +1,29 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col justify-center sm:justify-end items-center pt-6 sm:pt-0" style={{ backgroundImage: 'url(https://cdn.easyfrontend.com/pictures/sign-in-up/sign-in-up-1.png)', backgroundSize: 'cover' }}>
+        <div
+            // className="min-h-screen flex flex-col justify-center sm:justify-end items-center pt-6 sm:pt-0"
+            // style={{
+            //     backgroundImage: `url(${require("../image/LOGO_2.png")})`,
+            //     backgroundSize: "contain",
+            //     backgroundRepeat: "no-repeat",
+            //     backgroundPosition: "center top",
+            // }}
+            // className="min-h-screen flex flex-col justify-center sm:justify-end items-center pt-6 sm:pt-0"
+            // style={{
+            //     backgroundImage:
+            //         "url(https://cdn.easyfrontend.com/pictures/sign-in-up/sign-in-up-1.png)",
+            //     backgroundSize: "cover",
+            // }}
+            className="min-h-screen flex flex-col justify-center sm:justify-end items-center pt-6 sm:pt-0"
+            style={{
+                backgroundImage: `url(https://img.freepik.com/free-vector/healthcare-medical-with-cardiograph-line_1017-30390.jpg)`,
+                backgroundSize: "cover", // make the image cover the entire background
+                backgroundPosition: "center", // center the image horizontally and vertically
+            }}
+        >
             <div>
                 <Link>
                     <ApplicationLogo className="w-20 h-20" />
