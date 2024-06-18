bbfa0c5d6a5f1e9b7ca5ee8a79f30b9cfe1e6dd5
DeemzSaputra27 <165003278+DeemzSaputra27@users.noreply.github.com>
2024-06-18 18:39:09 +0700

    commit

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -11,75 +11,27 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
             title: "Technology",
             courses: 17,
         },
-        {
-            id: 2,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_2.png",
-            title: "Marketing",
-            courses: 22,
-        },
-        {
-            id: 3,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_4.png",
-            title: "Designing",
-            courses: 9,
-        },
-        {
-            id: 4,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_2.png",
-            title: "nomor2",
-            courses: 22,
-        },
-        {
-            id: 5,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_1.png",
-            title: "nomor3",
-            courses: 17,
-        },
-        {
-            id: 6,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_4.png",
-            title: "nomor4",
-            courses: 9,
-        },
-        {
-            id: 7,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_2.png",
-            title: "nomor5",
-            courses: 22,
-        },
-        {
-            id: 8,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_1.png",
-            title: "nomor6",
-            courses: 17,
-        },
-        {
-            id: 9,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_4.png",
-            title: "nomor7",
-            courses: 9,
-        },
-        {
-            id: 10,
-            image: "https://cdn.easyfrontend.com/pictures/featured/featured_10_2.png",
-            title: "nomor8",
-            courses: 22,
-        },
+    ]);
+
+    const [backgroundImage, setBackgroundImage] = useState([
+        "../image/bgspa.jpg",
+        "../image/bgyoga3.jpg",
+        "../image/bgrun.jpg",
     ]);
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            if (currentIndex === cards.length - 1) {
-                setCurrentIndex(0); // reset to first card
+            if (currentIndex === backgroundImage.length - 1) {
+                setCurrentIndex(0);
             } else {
                 setCurrentIndex(currentIndex + 1);
             }
-        }, 3000); // change every 3 seconds
+        }, 2000);
 
         return () => clearInterval(intervalId);
-    }, [cards, currentIndex]);
+    }, [backgroundImage, currentIndex]);
 
     const handleImageError = () => {
         document
@@ -96,82 +48,112 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
         <>
             <Head title="Welcome" />
 
-            <section className="ezy__header30 light relative overflow-hidden text-white bg-white dark:bg-[#0b1727] z-10">
-                <div
-                    className="ezy__header30-topbar py-6 md:py-10 bg-center bg-no-repeat bg-cover relative flex items-center justify-center z-[1]"
+            <section
+                className="ezy__header30 light relative overflow-hidden text-white bg-white dark:bg-[#0b1727] z-10"
+                style={{
+                    backgroundImage: `url(${backgroundImage[currentIndex]})`,
+                    backgroundSize: "100%",
+                    objectFit: "cover",
+                    transition: "background-image 1s ease-in-out",
+                }}
+            >
+                <nav
+                    className="fixed top-4 right-4 flex justify-end rounded-md p-2"
                     style={{
-                        backgroundImage: `url(../image/bgyoga.jpg)`,
-                        backgroundSize: "100%",
-                        objectFit: "cover",
+                        animation: "fadeIn 1s ease-in-out",
                     }}
                 >
-                    <nav className="fixed top-4 right-4 flex justify-end rounded-md p-2">
-                        {auth.user ? (
+                    {auth.user ? (
+                        <Link
+                            href={route("dashboard")}
+                            className="bg-gray-100 hover:bg-gray-200 dark:bg-[#0b1727] dark:hover:bg-gray-700 px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white rounded-md"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
                             <Link
-                                href={route("dashboard")}
-                                className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white rounded-md"
+                                href={route("login")}
+                                className="bg-gray-100 hover:bg-gray-200 dark:bg-[#0b1727] dark:hover:bg-gray-700 px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white rounded-md"
                             >
-                                Dashboard
+                                Log in
                             </Link>
-                        ) : (
-                            <>
-                                <Link
-                                    href={route("login")}
-                                    className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white rounded-md"
+                            <span className="px-3 py-2 text-[#0b1727]">|</span>
+                            <Link
+                                href={route("register")}
+                                className="bg-gray-100 hover:bg-gray-200 dark:bg-[#0b1727] dark:hover:bg-gray-700 px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white rounded-md"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </nav>
+                <div
+                    className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-60 -z-10"
+                    style={{
+                        animation: "fadeIn 0.5s ease-in-out",
+                    }}
+                />
+                <div className="container px-4 mx-auto">
+                    <ApplicationLogo />
+                    {/* content */}
+                    <div
+                        className="max-w-5xl flex justify-center text-center mx-auto mb-24"
+                        style={{
+                            animation: "fadeIn 0.5s ease-in-out",
+                        }}
+                    >
+                        <div className="mb-24">
+                            <h1
+                                className="text-3xl font-bold leading-tight md:text-6xl mb-6"
+                                style={{
+                                    animation: "fadeIn 0.5s ease-in-out",
+                                }}
+                            >
+                                Apa itu
+                                <span
+                                    className="text-blue-300 ml-2 my-4 text-6xl font-bold leading-tight"
+                                    style={{
+                                        animation: "fadeIn 0.5s ease-in-out",
+                                    }}
                                 >
-                                    Log in
-                                </Link>
-                                <span className="px-3 py-2 text-gray-800">
-                                    |
+                                    Vitalife
                                 </span>
-                                <Link
-                                    href={route("register")}
-                                    className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white rounded-md"
-                                >
-                                    Register
-                                </Link>
-                            </>
-                        )}
-                    </nav>
-                    {/* <ApplicationLogo className="flex justify-center mt-12" /> */}
-                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-60 -z-10" />
-                    <div className="container px-4 mx-auto">
-                        <div className="max-w-5xl flex justify-center text-center mx-auto">
-                            <div>
-                                <ApplicationLogo />
-                                <h1 className="text-3xl font-bold leading-tight md:text-6xl mb-6">
-                                    Apa itu
-                                    <span className="text-blue-300 ml-2 my-4 text-6xl font-bold leading-tight">
-                                        Vitalife
-                                    </span>
-                                    ?
-                                </h1>
-                                <div className="max-w-lg flex justify-center mx-auto">
-                                    <div className="text-center">
-                                        <p className="text-xl opacity-80 leading-snug">
-                                            Project Vitalife adalah project
-                                            pengembangan aplikasi mobile
-                                            "Vitalife" yang bertujuan untuk
-                                            meningkatkan wisata kesehatan dan
-                                            kebugaran di Indonesia. Aplikasi ini
-                                            akan membantu pengguna dalam
-                                            menemukan pusat yoga dan spa
-                                            terbaik, acara olahraga dan event
-                                            terbaru, berkonsultasi dengan
-                                            dokter, melacak perkembangan mereka,
-                                            dan mendapatkan umpan balik tentang
-                                            layanan aplikasi.
-                                        </p>
-                                    </div>
+                                ?
+                            </h1>
+                            <div
+                                className="max-w-lg flex justify-center mx-auto"
+                                style={{
+                                    animation: "fadeIn 0.5s ease-in-out",
+                                }}
+                            >
+                                <div className="text-center">
+                                    <p
+                                        className="text-xl opacity-80 leading-snug"
+                                        style={{
+                                            animation:
+                                                "fadeIn 0.5s ease-in-out",
+                                        }}
+                                    >
+                                        Project Vitalife adalah project
+                                        pengembangan aplikasi mobile "Vitalife"
+                                        yang bertujuan untuk meningkatkan wisata
+                                        kesehatan dan kebugaran di Indonesia.
+                                        Aplikasi ini akan membantu pengguna
+                                        dalam menemukan pusat yoga dan spa
+                                        terbaik, acara olahraga dan event
+                                        terbaru, berkonsultasi dengan dokter,
+                                        melacak perkembangan mereka, dan
+                                        mendapatkan umpan balik tentang layanan
+                                        aplikasi.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            {/* <footer className="py-16 text-center text-sm text-black dark:text-black">
-                Vitalife &copy; {new Date().getFullYear()}
-            </footer> */}
+
             <footer>
                 <section className="ezy__copyright10 light py-12 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white">
                     <div className="container px-4">
@@ -188,7 +170,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                         </div>
                                         <div>
                                             <p className="ml-3">
-                                                &#169; Copyright
+                                                &#169; Copyright{" "}
                                                 {new Date().getFullYear()}
                                             </p>
                                         </div>
