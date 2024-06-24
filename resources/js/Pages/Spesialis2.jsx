12d3e34ff08e3ec846ca2b8f64ae791aef471c8c
DeemzSaputra27 <165003278+DeemzSaputra27@users.noreply.github.com>
2024-06-24 12:29:11 +0700

    commit

--- resources/js/Pages/Spesialis2.jsx
+++ resources/js/Pages/Spesialis2.jsx
@@ -151,20 +151,6 @@ function Spesialis({ auth }) {
                 </div>
 
                 <div className="flex items-center pl-32 pt-8">
-                    {/* <div className="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center text-white font-bold mr-4">
-                        <svg
-                            className="w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </div> */}
                     <h3 className="text-3xl font-bold  text-gray-800">
                         Spesialist
                     </h3>
